b99a11d7982ef8cb480e20aeb60410b713ab2b1e
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-08-30 18:03:55 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Context from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Context>
     <App />
+    </Context>
+    
   </React.StrictMode>
 );
 
