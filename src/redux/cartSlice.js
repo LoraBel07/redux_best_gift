b88d01e23d9f95816f2f06022ca259c50a2f861d
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-12 17:02:34 -0400

    commit

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -7,27 +7,27 @@ export const cartSlice = createSlice({
 	},
 	reducers: {
 		addItemToCart: ( state, action ) => {
-			const timeId = new Date().getTime()
+			const timeId = new Date().getTime();
 			state.cartItems.push({
 				id: timeId,
 				giftId: action.payload.gift.id,
 				quantity: action.payload.quantity,
 				totalPrice: action.payload.quantity * action.payload.gift.price
-			})
+			});
 		},
 		removeItemFromCart: (state, action) => {
 			state.cartItems = state.cartItems.filter(
 				cartItem => cartItem.id !== action.payload.cartItemId
-			)
+			);
 		}
 	}
-})
+});
 
 export const getTotalPrice = state => {
 	return state.cart.cartItems.reduce((total, cartItems) => {
-		return cartItems.totalPrice + total
-	}, 0)
-}
+		return cartItems.totalPrice + total;
+	}, 0);
+};
 
 export const getCartItems = state => state.cart.cartItems;
 export const { addItemToCart, removeItemFromCart } = cartSlice.actions;
